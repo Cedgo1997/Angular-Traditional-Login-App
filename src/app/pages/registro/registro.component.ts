85526651dccad00618757ba9679e8d2f9306ac43
César Gonzalez <cedgo1997@gmail.com>
2020-07-10 23:20:51 -0400

    form basic validations

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { UserModel } from 'src/app/models/user.model';
+import { Component, OnInit } from "@angular/core";
+import { UserModel } from "src/app/models/user.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  selector: "app-registro",
+  templateUrl: "./registro.component.html",
+  styleUrls: ["./registro.component.css"],
 })
 export class RegistroComponent implements OnInit {
+  user: UserModel;
 
-  user:UserModel;
-
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-
     this.user = new UserModel();
-    this.user.email = 'cedgo1997@gmail.com';
-    
-   }
+    this.user.email = "cedgo1997@gmail.com";
+  }
 
+  onSubmit(form: NgForm) {
 
-   onSubmit(){
-     console.log
-     console.log(this.user)
-   }
+    if(form.invalid) {return;}
 
-
+    console.log("Formulario Enviado");
+    console.log(this.user);
+    console.log(form);
+  }
 }
