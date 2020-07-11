eafe14bf875e241a35aa0a57771b5e54fdf33572
César Gonzalez <cedgo1997@gmail.com>
2020-07-11 16:52:31 -0400

    Login alert added sweet alert

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { UserModel } from "src/app/models/user.model";
 import { AuthService } from "src/app/services/auth.service";
+import Swal from "sweetalert2";
 
 @Component({
   selector: "app-login",
@@ -19,9 +20,25 @@ export class LoginComponent implements OnInit {
     if (form.invalid) {
       return;
     }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      type: "info",
+      text: "Please, wait...",
+    });
+    Swal.showLoading();
+
     this.auth.logIn(this.user).subscribe(
-      (userData) => console.log(userData),
+      (userData) => {
+        console.log(userData);
+        Swal.close();
+      },
       (err) => {
+        Swal.fire({
+          type: "error",
+          title: 'Authentication Error',
+          text: err.error.error.message,
+        });
         console.log(err.error.error.message);
       }
     );
