d8509e69d520bbd4c2108097b1100cab7bc3bdd9
César Gonzalez <cedgo1997@gmail.com>
2020-07-11 14:40:58 -0400

    logIn method added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { UserModel } from 'src/app/models/user.model';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { UserModel } from "src/app/models/user.model";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+  constructor(private auth: AuthService) {}
 
-  constructor() { }
+  user: UserModel = new UserModel();
 
-  user:UserModel = new UserModel();
+  ngOnInit() {}
 
-  ngOnInit() { 
-
+  login(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.auth.logIn(this.user).subscribe(
+      (userData) => console.log(userData),
+      (err) => {
+        console.log(err.error.error.message);
+      }
+    );
   }
-
-
-  login(form:NgForm) {
-
-    if (form.invalid) {return;}
-    
-
-  }
-
-
 }
